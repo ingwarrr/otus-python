fff892363754a8c2a679fcde289a0bc21684c4c5
Igor Kulakov <ingwarrr.core@yandex.ru>
2020-12-14 00:18:40 +0300

    Last refactor (I mean it)
    - Replace if-else statement with dict (switch-style) in evenOddPrime()
    - Ridoff deep nested if-else statement in getPrime

--- hw_0_funcs/index.py
+++ hw_0_funcs/index.py
@@ -3,6 +3,7 @@
 
 from math import pow
 import time
+
 #  --- ЗАДАНИЕ 1 ---
 
 def showSpendedTime(func):
@@ -23,25 +24,22 @@ def powList(*args, pwr = 2):
 # вспомогательная функция для фильтрации
 def getPrime(num):
     deviders = [2,3]
-    if num > 1 and not num in deviders:
-        isPrime = True
-        for i in deviders:
-            if num % i == 0:
-                isPrime = False
-                break
-        if isPrime:
-            return num
+    isComplex = True
+
+    if num > 1 and not num in deviders:        
+        isComplex = [i for i in deviders if num % i == 0]        
+        return num if not isComplex else None
     elif num in deviders:
         return num
 
 # возвращает список отфильтрованных чисел
 @showSpendedTime
 def evenOddPrime(*args, **kwargs):
-    result = []
-    if kwargs['numType'] == 'even':
-        result = list(filter(lambda x: not x%2, args))        
-    elif kwargs['numType'] == 'odd':
-        result = list(filter(lambda x: x%2, args))
-    elif kwargs['numType'] == 'prime':
-        result = list(filter(getPrime, args))
-    return result
+    switch = {
+        'even': list(filter(lambda x: not x%2, args)),
+        'odd': list(filter(lambda x: x%2, args)),
+        'prime': list(filter(getPrime, args))
+    }
+    return switch[kwargs['numType']]
+    
+print(evenOddPrime(*[i for i in range(102)], numType = 'prime'))
